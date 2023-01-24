0eae4544f05832fe05970c7b7e4b7d0f5acdaab3
Balló György <ballogyor+arch@gmail.com>
2023-01-24 02:22:18 +0100

    Warn about transitive dependencies
    This commit change the way how namcap handles dependencies to meet with our recent guideline to not rely on transitive dependencies, as they might break, if one of the dependencies is updated.[1]
    
    Previously, namcap warned about dependencies if they are included but already satisfied, even if these are transitive dependencies. From now, namcap will warn if a dependency is not explicitly specified, and won't show a warning if these are already satisfied by other dependencies.
    
    This commit also simplifies and fixes various issues in the dependency handling logic, e.g.:
    - Strip the version numbers when searching what provides a package, otherwise it doesn't match any.
    - Remove some slow recursive dependency search. With the new logic we only need it in case of the explicitly specified dependencies.
    - Always show at least an info message when a dependency satisfied. Useful to see which components need a library.
    
    [1] https://wiki.archlinux.org/title/Arch_package_guidelines#Package_dependencies

--- Namcap/depends.py
+++ Namcap/depends.py
@@ -26,11 +26,6 @@ from Namcap.ruleclass import *
 import Namcap.tags
 from Namcap import package
 
-# manual overrides for relationships outside the normal metadata
-implicit_provides = {
-	'java-environment': ['java-runtime'],
-}
-
 def single_covered(depend):
 	"Returns full coverage tree of one package, with loops broken"
 	covered = set()
@@ -56,20 +51,10 @@ def getcovered(dependlist):
 		covered |= single_covered(d)
 	return covered
 
-def getcustom(pkginfo):
-	custom_name = {'^mingw-': ['mingw-w64-crt'],}
-	custom_depend = set()
-	for pattern in custom_name:
-		if re.search(pattern, pkginfo['name']):
-			custom_depend.update(custom_name[pattern])
-	return custom_depend
-
 def getprovides(depends):
 	provides = {}
 	for i in depends:
 		provides[i] = set()
-		if i in implicit_provides:
-			provides[i].update(implicit_provides[i])
 		pac = package.load_from_db(i)
 		if pac is None:
 			continue
@@ -81,30 +66,22 @@ def getprovides(depends):
 def analyze_depends(pkginfo):
 	errors, warnings, infos = [], [], []
 
-	# compute needed dependencies + recursive
+	# compute needed dependencies
 	dependlist = set(pkginfo.detected_deps.keys())
-	indirectdependlist = getcovered(dependlist)
-	customlist = getcustom(pkginfo)
-	for i in indirectdependlist:
-		infos.append(("dependency-covered-by-link-dependence %s", i))
-	needed_depend = dependlist | indirectdependlist | customlist
-	# the minimal set of needed dependencies
-	smartdepend = set(dependlist) - indirectdependlist
 
 	# Find all the covered dependencies from the PKGBUILD
 	pkginfo.setdefault("depends", [])
 	explicitdepend = set(pkginfo["depends"])
 	implicitdepend = getcovered(explicitdepend)
-	pkgbuild_depend = explicitdepend | implicitdepend
 
 	# Include the optdepends from the PKGBUILD
 	pkginfo.setdefault("optdepends", [])
 	optdepend = set(pkginfo["optdepends"])
-	optdepend |= getcovered(optdepend)
+	implicit_optdepend = getcovered(optdepend)
 
 	# Get the provides so we can reference them later
 	# smartprovides : depend => (packages provided by depend)
-	smartprovides = getprovides(smartdepend | explicitdepend)
+	smartprovides = getprovides(dependlist)
 
 	# The set of all provides for detected dependencies
 	allprovides = set()
@@ -112,30 +89,30 @@ def analyze_depends(pkginfo):
 		allprovides |= plist
 
 	# Do the actual message outputting stuff
-	for i in smartdepend:
-		# if the needed package is itself:
-		if i == pkginfo["name"]:
-			continue
-		# if the dependency is satisfied
-		if i in pkgbuild_depend:
-			continue
-		# if the dependency is pulled as a provider for some explicit dep
-		if smartprovides[i] & pkgbuild_depend:
-			continue
-		# the dependency is satisfied by a provides
-		if i in allprovides:
-			continue
+	for i in dependlist:
 		# compute dependency reason
 		reasons = pkginfo.detected_deps[i]
 		reason_strings = [Namcap.tags.format_message(reason) for reason in reasons]
 		reason = ', '.join(reason_strings)
+		# if the needed package is itself:
+		if i == pkginfo["name"]:
+			infos.append(("dependency-detected-satisfied %s (%s)", (i, reason)))
+			continue
+		# if the dependency is satisfied
+		if i in explicitdepend or smartprovides[i] & explicitdepend:
+			infos.append(("dependency-detected-satisfied %s (%s)", (i, reason)))
+			continue
 		# still not found, maybe it is specified as optional
-		if i in optdepend:
+		if i in optdepend or smartprovides[i] & optdepend:
 			warnings.append(("dependency-detected-but-optional %s (%s)", (i, reason)))
 			continue
-		# maybe, it is pulled as a provider for an optdepend
-		if smartprovides[i] & optdepend:
-			warnings.append(("dependency-detected-but-optional %s (%s)", (i, reason)))
+		# a needed dependency is transitive, it is implicitly satisfied
+		if i in implicitdepend or smartprovides[i] & implicitdepend:
+			warnings.append(("dependency-implicitly-satisfied %s (%s)", (i, reason)))
+			continue
+		# maybe, it is pulled as a transitive dependency for an optdepend
+		if i in implicit_optdepend or smartprovides[i] & implicit_optdepend:
+			warnings.append(("dependency-implicitly-satisfied-optional %s (%s)", (i, reason)))
 			continue
 		# now i'm pretty sure i didn't find it.
 		errors.append(("dependency-detected-not-included %s (%s)", (i, reason)))
@@ -144,18 +121,12 @@ def analyze_depends(pkginfo):
 		# multilib packages usually depend on their regular counterparts
 		if pkginfo["name"].startswith('lib32-') and i == pkginfo["name"].partition('-')[2]:
 			continue
-		# the package provides an important dependency
-		if i in smartprovides and (smartprovides[i] & smartdepend):
-			continue
-		# a needed dependency is superfluous it is implicitly satisfied
-		if i in implicitdepend and (i in smartdepend or i in indirectdependlist):
-			warnings.append(("dependency-already-satisfied %s", i))
 		# a dependency is unneeded if:
 		#   it is not in the depends as we see them
 		#   it does not pull some needed dependency which provides it
-		elif i not in needed_depend and i not in allprovides:
+		if i not in dependlist and i not in allprovides:
 			warnings.append(("dependency-not-needed %s", i))
-	infos.append(("depends-by-namcap-sight depends=(%s)", ' '.join(smartdepend) ))
+	infos.append(("depends-by-namcap-sight depends=(%s)", ' '.join(dependlist) ))
 
 	return errors, warnings, infos
 
